1e84920210bb3a339a42fcb8839f8bfc7019d7f1
ButCool <but.cool@foxmail.com>
2024-04-24 19:13:29 +0800

    Search

--- src/main/kotlin/cool/but/obsidian/api/ObsidianDocumentClient.kt
+++ src/main/kotlin/cool/but/obsidian/api/ObsidianDocumentClient.kt
@@ -1,13 +1,10 @@
 package cool.but.obsidian.api
 
-import com.dtflys.forest.annotation.Address
-import com.dtflys.forest.annotation.Body
-import com.dtflys.forest.annotation.Header
-import com.dtflys.forest.annotation.Put
-import com.dtflys.forest.annotation.Var
+import com.dtflys.forest.annotation.*
 import com.dtflys.forest.http.ForestResponse
 import com.fasterxml.jackson.databind.JsonNode
 import cool.but.obsidian.config.ObsidianServerAddress
+import cool.but.obsidian.data.dto.SimpleSearchResult
 import org.springframework.stereotype.Component
 
 @Component
@@ -20,6 +17,14 @@ interface ObsidianDocumentClient {
         @Body content: String,
         @Header headers: Map<String, String>,
     ): ForestResponse<JsonNode>
+
+
+    @Post("/search/simple")
+    @Address(source = ObsidianServerAddress::class)
+    fun searchDocumentByText(
+        @Query("query") query: String,
+        @Query("contextLength") contextLength: Int = 0,
+    ): List<SimpleSearchResult>
 
 }
 
---
+++ src/main/kotlin/cool/but/obsidian/data/dto/SimpleSearchResult.kt
@@ -0,0 +1,6 @@
+package cool.but.obsidian.data.dto
+
+class SimpleSearchResult {
+    var filename: String? = null
+    var score: Double? = null
+}
